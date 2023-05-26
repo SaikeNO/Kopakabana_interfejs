2c14ada02f9a992948b12e6a7069106fb1684baf
Mateusz Lengiewicz <mateusz.lengiewicz111@gmail.com>
2023-05-26 14:26:06 +0200

    Dodaj pliki projektów.

---
+++ Kopakabana_interfejs/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kopakabana
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void ButtonSedzia_Click(object sender, RoutedEventArgs e)
+        {
+            OpcjeSedziow opcjeSedziow = new OpcjeSedziow();
+            opcjeSedziow.ShowDialog();
+        }
+
+        private void ButtonDruzyna_Click(object sender, RoutedEventArgs e)
+        {
+            OpcjeDruzyn opcjeDruzyn = new OpcjeDruzyn();
+            opcjeDruzyn.ShowDialog();
+        }
+
+        private void ButtonRozrywka_Click_2(object sender, RoutedEventArgs e)
+        {
+            OpcjeRozgrywek opcjeRozgrywk = new OpcjeRozgrywek();
+            opcjeRozgrywk.ShowDialog();
+        }
+    }
+}
---
+++ Kopakabana_interfejs/OpcjeDruzynDalej.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kopakabana
+{
+    /// <summary>
+    /// Logika interakcji dla klasy OpcjeDruzynDalej.xaml
+    /// </summary>
+    public partial class OpcjeDruzynDalej : Window
+    {
+        public OpcjeDruzynDalej()
+        {
+            InitializeComponent();
+        }
+
+        private void DodajDruzyne_Click(object sender, RoutedEventArgs e)
+        {
+            DodajDruzyne dodajDruzyne = new DodajDruzyne();
+            dodajDruzyne.ShowDialog();
+        }
+    }
+}
