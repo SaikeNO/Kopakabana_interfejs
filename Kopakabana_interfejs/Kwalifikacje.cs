ad716dd5cba3e2dd9a9ae68857b6d1de8d3421b0
Mateusz Lengiewicz <mateusz.lengiewicz111@gmail.com>
2023-05-26 17:11:54 +0200

    Add kwalifikacje

--- Kopakabana_interfejs/Kwalifikacje.cs
+++ Kopakabana_interfejs/Kwalifikacje.cs
@@ -7,36 +7,26 @@ namespace Kopakabana
 		private Tabela Tabela { get; set; }
 		private Sport Sport { get; set; }
 		private List<Rozgrywka> listaRozgrywek = new();
-		private Kantorek kantorekSportu;
+
         public Kwalifikacje(Sport sport, List<Druzyna> listaDruzyn, Kantorek kantorek)
 		{
-			Sedzia sedzia;
 			Sport = sport;
             Tabela = new Tabela(listaDruzyn, sport);
-            kantorekSportu = kantorek.GetKantorekSportu(Sport);
-
-			/*
+			
 			for (int i = 0; i < listaDruzyn.Count; i++)
 			{
 				for (int j = i + 1; j < listaDruzyn.Count; j++)
 				{
-					Console.WriteLine(Rozgrywka.WyswietlDruzyny(listaDruzyn[i], listaDruzyn[j]));
-					Console.WriteLine(kantorekSportu);
-
-					sedzia = Kantorek.WybierzSedziego(kantorekSportu);
-
 					if (Sport is Siatkowka)
 					{
-						Sedzia[] sedziowiePomocniczy = Kantorek.WybierzSedziowPomocniczych(kantorekSportu, sedzia);
-						listaRozgrywek.Add(new RozgrywkaSiatkowka(listaDruzyn[i], listaDruzyn[j], sedzia, sedziowiePomocniczy[0], sedziowiePomocniczy[1]));
+						listaRozgrywek.Add(new RozgrywkaSiatkowka(listaDruzyn[i], listaDruzyn[j]));
 					}
 					else
 					{
-						listaRozgrywek.Add(new Rozgrywka(listaDruzyn[i], listaDruzyn[j], sedzia));
+						listaRozgrywek.Add(new Rozgrywka(listaDruzyn[i], listaDruzyn[j]));
 					}
 				}
 			}
-			*/
 		}
 		public void Rozegraj()
 		{
